b79ac0a7f37679150e3361f470a8762e209c545c
Prateek Vijay <prateek.vijay@ukg.com>
2024-08-13 14:23:04 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/signUp/sign-up.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,LoginComponent,SignUpComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
