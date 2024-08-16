b78ca7fca866757957c0799f335780e324f0dc0d
Prateek Vijay <prateek.vijay@ukg.com>
2024-08-16 12:07:37 +0530

    bug fix

--- src/app/components/signUp/sign-up.component.ts
+++ src/app/components/signUp/sign-up.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule],
+  imports: [],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
