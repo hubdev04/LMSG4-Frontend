49e6b884ac5273c342b0be38c1f1866e9d6dd20c
meelusumit <meelusumit.com>
2024-08-20 14:38:32 +0530

    adme

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { AdminDashboardComponent } from '../../pages/admin-dashboard/admin-dashboard.component';
 import { SignUpComponent } from '../signUp/sign-up.component';
+import { AdmeDashboardComponent } from '../../pages/adme-dashboard/adme-dashboard.component';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [AdminDashboardComponent,SignUpComponent],
+  imports: [AdminDashboardComponent,SignUpComponent,AdmeDashboardComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
