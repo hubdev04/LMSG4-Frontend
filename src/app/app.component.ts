8019617bd3e0fa4a830d6e027d58e2e4b41d1710
meelusumit <meelusumit.com>
2024-08-20 15:20:34 +0530

    updated Admin Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/signUp/sign-up.component';
 import { CardComponent } from './components/card/card.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { AdmeDashboardComponent } from './pages/adme-dashboard/adme-dashboard.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginComponent, SignUpComponent, CardComponent,AdminDashboardComponent,AdmeDashboardComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, SignUpComponent, CardComponent,AdminDashboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrected from styleUrl to styleUrls
 })
